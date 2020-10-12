acd6db50951679fe7d1632fb564f03db381a0ed5
Jonathan Gellin <jgellin@salesforce.com>
2020-10-12 12:03:18 -0400

    Save connection

---
+++ .devcontainer/saveconn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+[ ! -d "${HOME}/.sfdx" ] && mkdir "${HOME}/.sfdx"
+
+[ -z "${SFDX_COBU_USERNAME}" ] && exit 1
+
+connectionFile="${HOME}/.sfdx/${SFDX_COBU_USERNAME}.json"
+NL=$'\n'
+
+contents="{${NL}"
+contents="${contents}    \"username\": \"${SFDX_COBU_USERNAME}\""
+[ ! -z "${SFDX_COBU_ORGID}" ] && contents="${contents},${NL}    \"orgId\": \"${SFDX_COBU_ORGID}\""
+[ ! -z "${SFDX_COBU_ACCESSTOKEN}" ] && contents="${contents},${NL}    \"accessToken\": \"${SFDX_COBU_ACCESSTOKEN}\""
+[ ! -z "${SFDX_COBU_REFRESHTOKEN}" ] && contents="${contents},${NL}    \"refreshToken\": \"${SFDX_COBU_REFRESHTOKEN}\""
+[ ! -z "${SFDX_COBU_INSTANCEURL}" ] && contents="${contents},${NL}    \"instanceUrl\": \"${SFDX_COBU_INSTANCEURL}\""
+[ ! -z "${SFDX_COBU_LOGINURL}" ] && contents="${contents},${NL}    \"loginUrl\": \"${SFDX_COBU_LOGINURL}\""
+[ ! -z "${SFDX_COBU_CLIENTID}" ] && contents="${contents},${NL}    \"clientId\": \"${SFDX_COBU_CLIENTID}\""
+[ ! -z "${SFDX_COBU_ISDEVHUB}" ] && contents="${contents},${NL}    \"isDevHub\": ${SFDX_COBU_ISDEVHUB}"
+[ ! -z "${SFDX_COBU_CREATEDORGINSTANCE}" ] && contents="${contents},${NL}    \"createdOrgInstance\": \"${SFDX_COBU_CREATEDORGINSTANCE}\""
+[ ! -z "${SFDX_COBU_CREATED}" ] && contents="${contents},${NL}    \"created\": \"${SFDX_COBU_CREATED}\""
+[ ! -z "${SFDX_COBU_DEVHUBUSERNAME}" ] && contents="${contents},${NL}    \"devHubUsername\": \"${SFDX_COBU_DEVHUBUSERNAME}\""
+[ ! -z "${SFDX_COBU_EXPIRATIONDATE}" ] && contents="${contents},${NL}    \"expirationDate\": \"${SFDX_COBU_EXPIRATIONDATE}\""
+contents="${contents}${NL}}"
+
+echo "${contents}" > "${connectionFile}"
